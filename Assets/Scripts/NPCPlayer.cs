0568a60ec8b5d83a7be1660f583b5f238db24009
MalekEsprit <malek.jemni@esprit.tn>
2023-07-08 14:31:36 +0200

    added Npc player step back

--- Assets/Scripts/NPCPlayer.cs
+++ Assets/Scripts/NPCPlayer.cs
@@ -1,15 +1,15 @@
 using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class NPCPlayer : MonoBehaviour
 {
     public float detectionRadius = 5f; 
     public LayerMask enemyLayer;
     private AIDestinationSetter destinationSetter;
+    private NPCPlayerBehaviour nPCPlayerBehaviour;
 
     public LayerMask obstacleL;
     public Transform patrolPoint;
@@ -18,15 +18,26 @@ public class NPCPlayer : MonoBehaviour
     public float lkpoints;
 
 
+    private float stepBackDistance = 2f; 
+    private float minLerpTime = 1f; 
+    private float maxLerpTime = 1.5f;
+
+    private SpriteRenderer spriteRenderer;
+
+
     private void Awake()
     {
-        destinationSetter = GetComponent<AIDestinationSetter>();     
+        destinationSetter = GetComponent<AIDestinationSetter>();
+        nPCPlayerBehaviour = GetComponent<NPCPlayerBehaviour>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();  
     }
 
     private void Update()
     {
 
         if (!CheckForEnemy()) Moving();
+
+        ReachEnemy();
     }
 
     private bool CheckForEnemy()
@@ -43,14 +54,66 @@ public class NPCPlayer : MonoBehaviour
         return false;
     }
 
+
+    private void ReachEnemy()
+    {
+        if (CheckForEnemy())
+        {
+            Vector3 dist = transform.position - destinationSetter.target.position;
+            if (dist.magnitude < 1f)
+            {
+                Vector3 direction = dist.normalized;
+                Vector3 targetPosition = transform.position + direction * stepBackDistance;
+
+                float lerpTime = Random.Range(minLerpTime, maxLerpTime);
+                nPCPlayerBehaviour.AttackAnimaton();
+                StartCoroutine(MoveBackSmoothly(targetPosition, lerpTime));
+            }
+        }
+    }
+
+    private IEnumerator MoveBackSmoothly(Vector3 targetPosition, float lerpTime)
+    {
+        Vector3 startPosition = transform.position;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < lerpTime)
+        {
+            float t = Mathf.SmoothStep(0f, 1f, elapsedTime / lerpTime);
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPosition;
+    }
+
+
+
     private void Moving()
     {
         if (!wkSet) LookForNodes();
+
         if (wkSet)
         {
+            nPCPlayerBehaviour.MoveAnimaton(true);
             Transform destination = patrolPoint;
             destination.position = wkPoint;
             destinationSetter.target = destination;
+
+            // Turn towards movement direction
+            Vector2 movementDirection = (wkPoint - transform.position).normalized;
+            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            if (angle > 90f || angle < -90f)
+            {
+                // Flip the sprite if the angle is outside the range of -90 to 90 degrees
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
         }
 
         Vector3 dist = transform.position - wkPoint;
@@ -59,8 +122,18 @@ public class NPCPlayer : MonoBehaviour
             destinationSetter.target = null;
             wkSet = false;
         }
-
     }
+
+    private void TurnTowardsMovementDirection(Vector2 movementDirection)
+    {
+        if (movementDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+
 
     private void LookForNodes()
     {
@@ -72,7 +145,6 @@ public class NPCPlayer : MonoBehaviour
 
         if (!Physics2D.OverlapCircle(patrolPointPosition, 0.2f, obstacleL))
         {
-            Debug.Log("rgergr");
             wkSet = true;
         }
     }
