ec9c6b593059e31dffa174b62b4228f3a9e6d451
SelimDenizZayene <denizselimzayene@gmail.com>
2023-07-08 09:16:00 +0100

    Added IDamageable Interface and PlayerHealth Class

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerHealth : MonoBehaviour, IDamageable
+    {
+        public int health;
+
+        public static event Action OnPlayerDeath;
+
+        private void OnEnable()
+        {
+            OnPlayerDeath += PlayerDeath;
+        }
+
+        private void OnDisable()
+        {
+            OnPlayerDeath -= PlayerDeath;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            health = health - damage;
+            if (health <= 0)
+            {
+                OnPlayerDeath?.Invoke();
+            }
+        }
+
+        public void PlayerDeath()
+        {
+            //Todo: death and scene transitions
+        }
+    }
+}
