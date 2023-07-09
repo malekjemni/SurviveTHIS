07ec4fb108252ac1c5bf9fafc951f2968e1914db
SelimDenizZayene <denizselimzayene@gmail.com>
2023-07-09 11:47:47 +0100

    added player feedback strings

--- Assets/Scripts/PlayerInterestManager.cs
+++ Assets/Scripts/PlayerInterestManager.cs
@@ -10,17 +10,30 @@ namespace Assets.Scripts
 
         private float interestPerSecond = -1.0f;
 
+        [SerializeField]
+        private float baseInterestPerSecond = -1.0f;
+        [SerializeField]
+        private float interestPerKill = 0.5f;
+
         private float maxInterest = 100.0f;
         private float minInterest = -20.0f;
 
         public string feedBack;
 
+        [SerializeField]
+        private TimeManager timeManager;
+
         public static event Action OnPlayerSatisfied;
         public static event Action OnPlayerBored;
+        public static event Action<string> OnGameEnd;
 
         private void OnEnable()
         {
             PlayerHealth.OnPlayerDeath += PlayerDeathEnding;
+            OnPlayerBored += PlayerBoredEnding;
+            OnPlayerSatisfied += PlayerSatisfiedEnding;
+
+
             MobHealth.OnAnyMobDeath += GainInterestByKilling;
             PlayerLevelManager.OnLevelUp += LevelUpInterest;
         }
@@ -28,6 +41,10 @@ namespace Assets.Scripts
         private void OnDisable()
         {
             PlayerHealth.OnPlayerDeath -= PlayerDeathEnding;
+            OnPlayerBored -= PlayerBoredEnding;
+            OnPlayerSatisfied -= PlayerSatisfiedEnding;
+
+
             MobHealth.OnAnyMobDeath -= GainInterestByKilling;
             PlayerLevelManager.OnLevelUp -= LevelUpInterest;
         }
@@ -51,18 +68,67 @@ namespace Assets.Scripts
 
         private void GainInterestByKilling(int level)
         {
-            interestLevel += 0.5f;
+            interestLevel += interestPerKill;
         }
 
         private void LevelUpInterest(int level)
         {
-            interestPerSecond = -1 - level * 0.1f;
+            interestPerSecond = baseInterestPerSecond - level * 0.1f;
         }
 
         private void PlayerDeathEnding()
         {
-            //check time
-            //check interest level
+            if(timeManager.timer < 20.0f)
+            {
+                feedBack = "I think this game is not for me, it's too hard!";
+                OnGameEnd?.Invoke(feedBack);
+                return;
+            }
+                
+
+            else if(timeManager.timer < 60.0f)
+            {
+                feedBack = "the balance seems a bit off";
+                OnGameEnd?.Invoke(feedBack);
+                return;
+            }
+            
+
+            if (interestLevel < 0.0f)
+                feedBack = "Are those monsters too hard to kill?";
+
+            else if (interestLevel < 50.0f)
+                feedBack = "it's unfair that I died, but it was kinda fun";
+
+            else if (interestLevel < 80.0f)
+                feedBack = "this game is awesome!";
+
+            else
+                feedBack = "WOW! best game ever! Can't wait to start again!";
+
+            OnGameEnd?.Invoke(feedBack);
+        }
+
+        private void PlayerSatisfiedEnding()
+        {
+            if (timeManager.timer < 60.0f)
+                feedBack = "This was Awesome. a little fast paced but fun.";
+
+            else
+                feedBack = "This game is awesome, I killed so many monsters. But I feel like I had enough.";
+
+            OnGameEnd?.Invoke(feedBack);
+        }
+
+        private void PlayerBoredEnding()
+        {
+            if (timeManager.timer < 21.0f)
+                feedBack = "Where did the monsters go??!";
+
+            else
+                feedBack = "Alright, I had enough. This game is boring!";
+
+            OnGameEnd?.Invoke(feedBack);
         }
     }
 }
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TimeManager : MonoBehaviour
+    {
+        public float timer;
+
+        [SerializeField] private TextMeshProUGUI minutes1;
+        [SerializeField] private TextMeshProUGUI minutes2;
+        [SerializeField] private TextMeshProUGUI seconds1;
+        [SerializeField] private TextMeshProUGUI seconds2;
+
+        private bool gameRunning = true;
+
+        private void OnEnable()
+        {
+            PlayerInterestManager.OnGameEnd += StopTimer;
+        }
+
+        private void OnDisable()
+        {
+            PlayerInterestManager.OnGameEnd -= StopTimer;
+        }
+
+        private void Start()
+        {
+            timer = 0.0f;
+        }
+
+        private void Update()
+        {
+            if (!gameRunning) return;
+
+            timer += Time.deltaTime;
+            UpdateTimeDisplay(timer);
+        }
+
+        private void UpdateTimeDisplay(float time)
+        {
+            float minutes = Mathf.FloorToInt(time / 60);
+            float seconds = Mathf.FloorToInt(time % 60);
+
+            string currentTime = string.Format("{00:00}{1:00}",minutes,seconds);
+
+            minutes1.text = currentTime[0].ToString();
+            minutes2.text = currentTime[1].ToString();
+            seconds1.text = currentTime[2].ToString();
+            seconds2.text = currentTime[3].ToString();
+        }
+        private void StopTimer(string obj)
+        {
+            gameRunning = false;
+        }
+    }
+}
